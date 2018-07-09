3730a0f7188489d3e2551486d67d0249d7b27919
Sinclert <Sinclert@hotmail.com>
2018-07-09 17:32:42 +0200

    index: first steps + public folder

---
+++ reana-ui/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Login from './login-page/Login';
+
+ReactDOM.render(
+	<Login />,
+	document.getElementById('root')
+);
