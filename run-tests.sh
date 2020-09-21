0a5de35151bf42a3a963e0bb6bec29c663a6c378
Marco Vidal García <marco.vidal@cern.ch>
2020-09-21 13:54:14 +0200

    tests: exit on error

--- run-tests.sh
+++ run-tests.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 #
 # This file is part of REANA.
-# Copyright (C) 2018 CERN.
+# Copyright (C) 2020 CERN.
 #
 # REANA is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 
+# Quit on errors
+set -o errexit
+
+# Quit on unbound symbols
+set -o nounset
+
 sphinx-build -qnN docs docs/_build/html
-prettier reana-ui/src/**/*.js --list-different
+prettier reana-ui --check
 docker build -t reanahub/reana-ui .
