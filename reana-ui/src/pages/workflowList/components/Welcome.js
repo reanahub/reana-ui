ec9800e993a36dc03bd4e38544168e93302aeb66
Marco Donadoni <marco.donadoni@cern.ch>
2022-11-21 16:11:06 +0100

    ui: use 24-hour clock to format times

--- reana-ui/src/pages/workflowList/components/Welcome.js
+++ reana-ui/src/pages/workflowList/components/Welcome.js
@@ -2,7 +2,7 @@
   -*- coding: utf-8 -*-
 
   This file is part of REANA.
-  Copyright (C) 2020 CERN.
+  Copyright (C) 2020, 2022 CERN.
 
   REANA is free software; you can redistribute it and/or modify it
   under the terms of the MIT License; see LICENSE file for more details.
@@ -130,7 +130,7 @@ export function WelcomeNoTokenMsg() {
       </p>
       <Button content="Token requested" disabled />
       <small className={styles.requested}>
-        <em>{moment.utc(tokenRequestedAt).format("YYYY-MM-DDThh:mm:ss")}</em>
+        <em>{moment.utc(tokenRequestedAt).format("YYYY-MM-DDTHH:mm:ss")}</em>
       </small>
     </div>
   ) : (
--- reana-ui/src/util.js
+++ reana-ui/src/util.js
@@ -98,7 +98,7 @@ function parseWorkflowRetentionRules(workflow) {
           ? getTimeBeforeExecution(applyOn, currentTime)
           : null;
         return {
-          applyOn: applyOn ? applyOn.format("YYYY-MM-DDThh:mm:ss") : null,
+          applyOn: applyOn ? applyOn.format("YYYY-MM-DDTHH:mm:ss") : null,
           timeBeforeExecution,
           retentionDays: retention_days,
           workspaceFiles: workspace_files,
