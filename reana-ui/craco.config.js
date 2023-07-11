78153048f3479c32d56b46ddb0343b1af9fa4b64
Marco Donadoni <marco.donadoni@cern.ch>
2023-07-11 16:42:18 +0200

    config: proxy requests to backend when using dev server
    Configure the development server to proxy `/api` requests to
    `$REANA_SERVER_URL`, instead of making direct requests to
    `$REANA_SERVER_URL` from the client. This means that all the requests,
    including the ones to the REST API, are now made to the same origin.
    This improves the situation with regard to mechanisms like CORS or
    SameSite cookies.
    
    Closes #320

--- reana-ui/craco.config.js
+++ reana-ui/craco.config.js
@@ -15,4 +15,20 @@ module.exports = {
       },
     },
   ],
+  devServer: {
+    proxy: [
+      {
+        context: [
+          "/api/**",
+          // this matches URLs to Jupyter notebooks (but also a bit more)
+          "/+([a-f0-9])-+([a-f0-9])-+([a-f0-9])-+([a-f0-9])-+([a-f0-9])/**",
+        ],
+        // `localhost` is replaced by `127.0.0.1` as sometimes it resolves to `::1` (IPv6 instead of IPv4)
+        target: process.env.REANA_SERVER_URL?.replace("localhost", "127.0.0.1"),
+        secure: false,
+        // needed to proxy websockets to Jupyter notebooks
+        ws: true,
+      },
+    ],
+  },
 };
--- reana-ui/src/config.js
+++ reana-ui/src/config.js
@@ -2,13 +2,13 @@
   -*- coding: utf-8 -*-
 
   This file is part of REANA.
-  Copyright (C) 2020, 2022 CERN.
+  Copyright (C) 2020, 2022, 2023 CERN.
 
   REANA is free software; you can redistribute it and/or modify it
   under the terms of the MIT License; see LICENSE file for more details.
 */
 
-export const api = process.env.REACT_APP_SERVER_URL || window.location.origin;
+export const api = window.location.origin;
 
 /**
  * Valid Launch-on-REANA querystring parameter keys.
